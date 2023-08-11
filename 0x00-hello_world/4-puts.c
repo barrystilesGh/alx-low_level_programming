26410d201352e6ce0935ee7a8d13288553610b4a
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-08-11 08:05:11 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
 *main a c script that print a function call push
-
+*
 *return: 0;
 */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
