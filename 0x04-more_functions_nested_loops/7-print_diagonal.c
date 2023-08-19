91b1bcde69d55b28ea74b732a7878eab0c49787e
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-08-18 22:37:22 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-print_diagonal.c -o 7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,53 +1,48 @@
 #include "main.h"
 
-
 /**
-
- * print_diagonal - draws a diagonal line on the terminal
-
- * @n: number of times the character \ should be printed
-
- */
+* print_diagonal - draws a diagonal line on the terminal
+* @n: number of times the character \ should be printed
+*/
 
 void print_diagonal(int n)
 
 {
 
- if (n <= 0)
+	if (n <= 0)
 
- {
+	{
 
- _putchar('\n');
+	_putchar('\n');
 
- } else
+	} else
 
- {
+	{
 
- int i, j;
+	int i, j;
 
+	for (i = 0; i < n; i++)
 
- for (i = 0; i < n; i++)
+	{
 
- {
+	for (j = 0; j < n; j++)
 
- for (j = 0; j < n; j++)
+	{
 
- {
+	if (j == i)
 
- if (j == i)
+	_putchar('\\');
 
- _putchar('\\');
+	else if (j < i)
 
- else if (j < i)
+	_putchar(' ');
 
- _putchar(' ');
+	}
 
- }
+	_putchar('\n');
 
- _putchar('\n');
+	}
 
- }
-
- }
+	}
 
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,66 +1,58 @@
 #include <stdio.h>
 
-
 /**
-
- * main - prints the numbers from 1 to 100, followed by a new line
-
- * but for multiples of three prints Fizz instead of the number
-
- * and for the multiples of five prints Buzz
-
- * Return: Always 0 (Success)
-
- */
+* main - prints the numbers from 1 to 100, followed by a new line
+* but for multiples of three prints Fizz instead of the number
+* and for the multiples of five prints Buzz
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
 {
 
- int i;
+	int i;
 
+	for (i = 1; i <= 100; i++)
 
- for (i = 1; i <= 100; i++)
+	{
 
- {
+	if (i % 3 == 0 && i % 5 != 0)
 
- if (i % 3 == 0 && i % 5 != 0)
+	{
 
- {
+	printf(" Fizz");
 
- printf(" Fizz");
+	} else if (i % 5 == 0 && i % 3 != 0)
 
- } else if (i % 5 == 0 && i % 3 != 0)
+	{
 
- {
+	printf(" Buzz");
 
- printf(" Buzz");
+	} else if (i % 3 == 0 && i % 5 == 0)
 
- } else if (i % 3 == 0 && i % 5 == 0)
+	{
 
- {
+	printf(" FizzBuzz");
 
- printf(" FizzBuzz");
+	} else if (i == 1)
 
- } else if (i == 1)
+	{
 
- {
+	printf("%d", i);
 
- printf("%d", i);
+	} else
 
- } else
+	{
 
- {
+	printf(" %d", i);
 
- printf(" %d", i);
+	}
 
- }
+	}
 
- }
+	printf("\n");
 
- printf("\n");
-
-
- return (0);
+	return (0);
 
 }
