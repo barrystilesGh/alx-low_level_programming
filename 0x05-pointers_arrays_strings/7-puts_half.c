d54e519565ab5f6d21262ddd030edce383e16d5e
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-08-22 19:43:57 -0700

    “6-puts2.c”

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,7 @@ void puts_half(char *str)
 	longi = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
-
-		longi++;
+	longi++;
 
 	n = (longi / 2);
 
@@ -22,6 +21,7 @@ void puts_half(char *str)
 		n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
+
 		_putchar(str[a]);
 		_putchar('\n');
 }
