9fe33f25a08d622e2293549fa2971817d4ffb3b5
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-08-31 22:52:50 -0700

    corrections of betty errors

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,35 +1,28 @@
 #include "main.h"
-
 /**
-
  * _strlen_recursion - Returns the length of a string.
-
  * @s: The string to be measured.
-
  *
-
  * Return: The length of the string.
-
  */
 
 int _strlen_recursion(char *s)
+{
+
+	int longit = 0;
+
+
+	if (*s)
 
 {
 
-int longit = 0;
+	longit++;
 
-
-if (*s)
-
-{
-
-longit++;
-
-longit += _strlen_recursion(s + 1);
+	longit += _strlen_recursion(s + 1);
 
 }
 
 
-return (longit);
+	return (longit);
 
 }
